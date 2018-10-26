274be3e85b0babbba529d9ee4a7179c07ea684d8
und3r <34609672+und3rm4rv3l@users.noreply.github.com>
2018-10-26 15:53:59 -0300

    Update aqua.js

--- cmds/aqua.js
+++ cmds/aqua.js
@@ -1,7 +1,7 @@
-exports.run = (Pietra, message, args) => {
+exports.run = (Pietra, message, suffix) => {
 if (!['244489368717230090'].includes(message.author.id)) return;
     try {
-      var code = args;
+      var code = suffix;
       var evalued = eval(code);
 
       if (typeof evalued !== "string")
@@ -10,9 +10,10 @@ if (!['244489368717230090'].includes(message.author.id)) return;
      message.channel.sendCode("x1", evalued);
     } catch(err) {
           message.channel.send({embed: 
-          {description: ` \`ERROR\`\n**Código >**\n\`\`\`Js\n${args}\n\`\`\`\n\`\`\`xl\n${err}\n\`\`\` `,
+          {description: ` \`\`\`ERROR\`\`\` **Código >** \`\`\`Js\n${suffix}\n\`\`\` \`\`\`xl\n${err}\n\`\`\` `,
           color: 0x4959e9}});
   
 
         
+}
 }};
